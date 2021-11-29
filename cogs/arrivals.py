639903c26ddf481d90dd6a3fedb1100e1e66da1c
Markanito <58856275+Markanito@users.noreply.github.com>
2021-11-30 00:11:45 +0100

    True fix for unboud local variable
    Fixed unbound local variable where ETA couldn't be calculated. Error in time converter logic

--- cogs/arrivals.py
+++ cogs/arrivals.py
@@ -70,53 +70,36 @@ class arrivals(Cog):
                             destairport = item['flight_plan']['arrival']
                             route = item['flight_plan']['route']
 
+                            #ETA calculation, do not mess with this or it can cause wrong ETA calculations
+                            if item['groundspeed'] > 40:
+                                time = int(distance) / int(item['groundspeed'])
+                                hour = int(time)
+                                arrival_hour = utc.hour + hour
+                                arrival_minute = utc.minute + int((time - int(time))*60)
+                                arrival_time = f"{arrival_hour}:{arrival_minute}z"
+                                if arrival_minute <10:
+                                    arrival_time = f"{arrival_hour}:0{arrival_minute}z"
 
-                            #Status converter
-                            if int(item['groundspeed']) < 50:
-                                if distance < 10:
-                                    status = f"Arrived at destination"
-                                    color = Colour.green()
+                                elif 10 < arrival_minute < 59:
+                                    arrival_time = f"{arrival_hour}:{arrival_minute}z"
+
+                                elif arrival_minute > 59:
+                                    add_hour = int(arrival_minute / 60)
+                                    arrival_minute = int(((arrival_minute / 60) - add_hour) * 60)
+                                    arrival_hour = arrival_hour + add_hour
                                 else:
-                                    status = f"Preparing for the flight"
-                                    color = Colour.dark_orange()
+                                    arrival_time= f"{arrival_hour}:{arrival_minute}z"   
+
+                                if arrival_hour > 23:
+                                    days = int(arrival_hour / 24)
+                                    arrival_hour = (arrival_hour - (days * 24))
+                                    if days == 1:
+                                        if arrival_minute < 10:
+                                            arrival_time = f"{arrival_hour}:0{arrival_minute}z tomorrow"
+                                        else:
+                                            arrival_time = f"{arrival_hour}:{arrival_minute}z tomorrow"
                             else:
-                                status = f"On the way"
-                                color = Colour.blue()
-
-                            #Time Converter, please don't fuck with this
-                            try:
-                                if int(item['groundspeed']) > 50:
-                                    
-                                    time = distance / int(item['groundspeed'])
-                                    hour = int(time)
-                                    arrival_hour = utc.hour + hour
-                                    arrival_minute = utc.minute + int((time - int(time))*60)
-                                    if arrival_minute <10:
-                                        arrival_time = f"{arrival_hour}:0{arrival_minute}z"
-
-                                    elif 10 < arrival_minute < 59:
-                                        arrival_time = f"{arrival_hour}:{arrival_minute}z"
-
-                                    elif arrival_minute > 59:
-                                        add_hour = int(arrival_minute / 60)
-                                        arrival_minute = int(((arrival_minute / 60) - add_hour) * 60)
-                                        arrival_hour = arrival_hour + add_hour
-                                            
-                                        if arrival_hour > 23:
-                                            days = int(arrival_hour / 24)
-                                            arrival_hour = (arrival_hour - (days * 24))
-                                            if days == 1:
-                                                if arrival_minute < 10:
-                                                    arrival_time = f"{arrival_hour}:0{arrival_minute}z tomorrow"
-                                                else:
-                                                    arrival_time = f"{arrival_hour}:{arrival_minute}z tomorrow"
-                                    else:
-                                        arrival_time= f"{arrival_hour}:{arrival_minute}z"
-                                else:
-                                    arrival_time = f"Plane is on the ground."
-                            except:
-                                arrival_time = f"Plane is on the ground"
-
+                                arrival_time = f"Unknown"
                             
                             #Status converter
                             if int(item['groundspeed']) < 70:
